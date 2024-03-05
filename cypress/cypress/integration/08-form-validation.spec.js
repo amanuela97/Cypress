b3a90a0c7caee45fb2006057bda995459c5f4671
amanuela97 <babyzewdie@gmail.com>
2024-03-05 21:24:23 +0200

    test generating, form validations, cypress tasks (tasks run on server side), and cypress reusable commands

--- cypress/cypress/integration/08-form-validation.spec.js
+++ cypress/cypress/integration/08-form-validation.spec.js
@@ -3,11 +3,26 @@
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-submit"]').as('submit');
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submit').click();
+    cy.get('[data-test="sign-up-email"]:invalid')
+      .invoke('prop', 'validity')
+      .its('valueMissing')
+      .should('be.true');
+  });
 
-  it('should require that the email actually be an email address', () => {});
+  it('should require that the email actually be an email address', () => {
+    cy.get('[data-test="sign-up-email"]').type('manu@gmail.com');
+    cy.get('@submit').click();
+    cy.get('[data-test="sign-up-email"]')
+      .invoke('val')
+      .then((sometext) => {
+        cy.wrap(sometext).should('match', /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+      });
+  });
 
   it('should require a password when the email is present', () => {});
 });
--- cypress/cypress/integration/09-echo-chamber-with-tasks.spec.js
+++ cypress/cypress/integration/09-echo-chamber-with-tasks.spec.js
@@ -1,23 +1,23 @@
 /// <reference types="cypress" />
 
+import '../support/commands';
+
 const user = {
-  email: `${Date.now()}@example.com`,
+  email: `first@example.com`,
   password: 'password123',
 };
 
 describe('Sign Up', () => {
+  beforeEach(() => {
+    cy.task('reset');
+  });
+
   it('should successfully create a user when entering an email and a password', () => {
     // Sign Up
-    cy.visit('/echo-chamber/sign-up');
-    cy.get('[data-test="sign-up-email"]').type(user.email);
-    cy.get('[data-test="sign-up-password"]').type(user.password);
-    cy.get('[data-test="sign-up-submit"]').click();
+    cy.signUp(user);
 
     // Sign In
-    cy.visit('/echo-chamber/sign-in');
-    cy.get('[data-test="sign-in-email"]').type(user.email);
-    cy.get('[data-test="sign-in-password"]').type(user.password);
-    cy.get('[data-test="sign-in-submit"]').click();
+    cy.signIn(user);
 
     cy.location('pathname').should('contain', '/echo-chamber/posts');
     cy.contains('Signed in as ' + user.email);
